26846d4d4c3b2827949bcffff1bad7aea700c9c6
agustinbie <agustinbielewicz@hotmail.com>
2023-09-19 00:24:22 -0300

    dev vs prod enviroment

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//keys.js contiene la logica para elegir el set de keys a usar dependiendo si estamos en production o developer enviroment
+
+if (process.env.NODE_ENV === "production") {
+    //we are in production - return the prod set of keys
+    module.exports = require("./prod");
+}else {
+    //we are in development  - return de dev keys!
+    module.exports = require("./dev") ;//exporta el archivo dev.js que esta en el mismo directorio de la compu local (en produccion no existe por el git ignore)
+}//no hace falta cambiar nada del resto de codigo del server, por mas que hagan require keys.js la logica se ejecuta y exporta las keys apropiadas
--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const {Schema} = mongoose; //es equivalente a la linea 2
 
 const userSchema = new Schema({
-    googleId: String, //el id de google eran todos numeros pero son provistos dentro de un string 
+    googleId: String, //el id de google eran todos numeros pero son provistos dentro de un string (no es el googleID de keys en config, es el id de cada usuario que se loguea)
     })
 
 mongoose.model("users", userSchema); //este comando crea una coleccion llamada "users" usando el Schema que creamos mas arriba
