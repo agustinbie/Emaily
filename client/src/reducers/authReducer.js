c3c137d1aa9fa6f8518411e41e8c460c177324c3
agustinbie <agustinbielewicz@hotmail.com>
2023-09-27 23:28:09 -0300

    authReducer return values

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,12 @@
-export default function(state = {}, action) {
-    console.log(action)
+import { FETCH_USER } from "../actions/types";
+
+
+
+export default function(state = null, action) { //state = null para mostrarlo asi hasta que se complete la request de /api/current_user
+    //console.log(action)
     switch (action.type) {
+        case FETCH_USER:
+        return action.payload || false; // si el usuario no está logueado no hay data en la respuesta y devuelve un string vacio que no queremos que se muestre. Queremos mostrar false, entonces agregamos el comparativo || para que javascript muestre false en vez de un string vacio. Un string vacio equivale a false en una comparativa. false or false = false
         default:
             return state;
     }
