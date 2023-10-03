313d924bcdf46e7191d7395fe0631040a3a2cda9
agustinbie <agustinbielewicz@hotmail.com>
2023-10-02 23:52:23 -0300

    redirectin login and logout

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -13,7 +13,7 @@ renderContent() {
             return <li><a href="/auth/google">Login With Google</a></li>;
            
         default:
-            return <li><a>Logout</a></li>;
+            return <li><a href="/api/logout">Logout</a></li>;
     }
 }
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,12 +6,15 @@ module.exports = (app) => {
 app.get("/auth/google", passport.authenticate("google", {scope: ["profile","email"]}));
                      //.authenticate usa el primer parametro "google" por defecto para referince a la estrategia de autenticacion de google Oauth. El segundo parametro es un objeto que contiene el scope con la info que queremos obtener del usuario de google
 
-app.get("/auth/google/callback", passport.authenticate("google")); //si bien este handler es parecido al line19, en esta request va a venir el codigo de google ya que el usuario dió permiso, GoogleStrategy reconoce este codigo y actua distinto.
+app.get("/auth/google/callback", passport.authenticate("google"), (req, res) => {
+    res.redirect("/surveys");
+}); //si bien este handler es parecido al line19, en esta request va a venir el codigo de google ya que el usuario dió permiso, GoogleStrategy reconoce este codigo y actua distinto.
 //con este handler ahora si probas localhost:5000/auth/google y das permiso, la pagina va a estar esperando pero nos envia el codigo que GoogleStrategy lo toma como el Access Token que pusimos en la linea 12 y lo logue ane la consola (flow raro entre lineas)
 
 app.get("/api/logout", (req, res) => {
     req.logout(); //logout() es una funcion que provee passport para matar el ID que viene en la cookie
-    res.send(req.user); //si vas a esta ruta, la pantalla aparece vacia porque user ya no esta identificado, lo mismo con /api/current_user
+    //res.send(req.user); //si vas a esta ruta, la pantalla aparece vacia porque user ya no esta identificado, lo mismo con /api/current_user
+    res.redirect("/");
 })
 
 
